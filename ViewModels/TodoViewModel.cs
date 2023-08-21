0ae27bfb4258ad0b834b97684c68e2a00879ea31
José <josebeto1113@gmail.com>
2023-08-20 22:17:50 -0300

    Initial Commit

---
+++ ViewModels/TodoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeuTodo.ViewModels
+{
+    public class TodoViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+    }
+}
